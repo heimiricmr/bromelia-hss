f903550ccd8856c904b2560ddad138366df501d7
henriquemr <heimiricmr@gmail.com>
2022-11-02 16:24:46 -0300

    fix(app): Update CounterDB class to avoid Redis connection errors

--- hss_app/app/counters.py
+++ hss_app/app/counters.py
@@ -1,15 +1,29 @@
 import redis
 
+from config import Config
+
 
 class CounterDB(redis.Redis):
     def __init__(self, **kwargs):
-        super().__init__(self, **kwargs)
+        super().__init__(**kwargs)
 
     def incr(self, name, amount=1):
         try:
-            return super().incr(name, amount)
-        except:
+            super().incr(name, amount)
+        except Exception as e:
             return None
-        
 
-app_counterdb = CounterDB(password="eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81")
+    def decr(self, name, amount=1):
+        try:
+            super().decr(name, amount)
+        except Exception as e:
+            return None
+
+    def get(self, name):
+        try:
+            return super().get(name)
+        except Exception as e:
+            return None
+
+
+app_counterdb = CounterDB(host=Config.cache_ip_address, password="eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81", socket_connect_timeout=2)
