5aec717b1d1d0a367f70252c85c9fbb759202b32
henriquemr <heimiricmr@gmail.com>
2023-11-03 14:16:26 -0300

    ci(app): integrate snmp into docker-compose deploy

--- hss_app/snmp/counters.py
+++ hss_app/snmp/counters.py
@@ -12,9 +12,12 @@
 #: 3rd-party dependencies
 import redis
 
+#: project modules
+from utils import get_cache_ip_address
+
 
 class CounterDB(redis.Redis):
     def __init__(self, **kwargs):
         redis.Redis.__init__(self, **kwargs)
 
-app_counterdb = CounterDB(password="eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81")
+app_counterdb = CounterDB(host=get_cache_ip_address(), password="eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81", socket_connect_timeout=2)
--- hss_app/snmp/utils.py
+++ hss_app/snmp/utils.py
@@ -11,6 +11,9 @@
 
 #: Python standard libs
 import os
+import re
+import subprocess
+import socket
 
 
 def convert(oid):
@@ -30,7 +33,32 @@ def is_docker():
         os.path.isfile(path) and any('docker' in line for line in open(path))
     )
 
+
+def get_default_ip_address() -> str:
+    try:
+        result = subprocess.run(["ip", "route", "get", "1"], capture_output=True, text=True)
+        for line in result.stdout.splitlines():
+            pattern = re.findall(r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\suid", line)
+            if pattern:
+                return pattern[0]
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+
 def get_host_ip_address():
     if is_docker():
         return "0.0.0.0"
-    return "localhost"
+    return get_default_ip_address()
+    
+
+def get_cache_ip_address():
+    try:
+        cache_hostname = socket.gethostbyname_ex("hss_app-cache")
+        print(f"Resolved hss_app-cache FQDN: {cache_hostname}")
+        return "hss_app-cache"
+
+    except socket.gaierror:
+        print("Not found hss_app-cache FQDN, fallback to default ...")
+        return "127.0.0.1"
